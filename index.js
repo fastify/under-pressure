869518855e59ae360a571bcd00610d7d493e3be5
delvedor <tommydelved@gmail.com>
2017-11-01 00:13:01 +0100

    Updated plugin - added maxHeapUsedBytes and maxRssBytes - removed loopbench dependency

--- index.js
+++ index.js
@@ -1,36 +1,91 @@
 'use strict'
 
 const fp = require('fastify-plugin')
-const Loopbench = require('loopbench')
 
 function underPressure (fastify, opts, next) {
   opts = opts || {}
-  opts.maxEventLoopDelay = opts.maxEventLoopDelay || {}
 
-  const loopbench = Loopbench(opts.maxEventLoopDelay)
-  const serviceUnavailableError = new Error(opts.maxEventLoopDelay.message || 'Service Unavailable')
-  const retryAfter = opts.maxEventLoopDelay.retryAfter || 10
+  const sampleInterval = opts.sampleInterval || 5
+  const maxEventLoopDelay = opts.maxEventLoopDelay || 0
+  const maxHeapUsedBytes = opts.maxHeapUsedBytes || 0
+  const maxRssBytes = opts.maxRssBytes || 0
 
-  fastify.decorate('loopbench', loopbench)
+  const checkMaxEventLoopDelay = maxEventLoopDelay > 0
+  const checkMaxHeapUsedBytes = maxHeapUsedBytes > 0
+  const checkMaxRssBytes = maxRssBytes > 0
+
+  var heapUsed = 0
+  var rssBytes = 0
+  var eventLoopDelay = 0
+  var lastCheck = now()
+  fastify.decorate('memoryUsage', memoryUsage)
+
+  if (checkMaxEventLoopDelay === false &&
+      checkMaxHeapUsedBytes === false &&
+      checkMaxRssBytes === false) {
+    return next()
+  }
+
+  const serviceUnavailableError = new Error(opts.message || 'Service Unavailable')
+  const retryAfter = opts.retryAfter || 10
+
   fastify.addHook('onRequest', onRequest)
   fastify.addHook('onClose', onClose)
 
+  const timer = setInterval(updateMemoryUsage, sampleInterval)
+  function updateMemoryUsage () {
+    var mem = process.memoryUsage()
+    heapUsed = mem.heapUsed
+    rssBytes = mem.rss
+    var toCheck = now()
+    eventLoopDelay = toCheck - lastCheck - sampleInterval
+    lastCheck = toCheck
+  }
+
   function onRequest (req, res, next) {
-    if (loopbench.overLimit) {
-      res.statusCode = 503
-      res.setHeader('Retry-After', retryAfter)
-      next(serviceUnavailableError)
+    if (checkMaxEventLoopDelay && eventLoopDelay > maxEventLoopDelay) {
+      sendError(res, next)
       return
     }
+
+    if (checkMaxHeapUsedBytes && heapUsed > maxHeapUsedBytes) {
+      sendError(res, next)
+      return
+    }
+
+    if (checkMaxRssBytes && rssBytes > maxRssBytes) {
+      sendError(res, next)
+      return
+    }
+
     next()
   }
 
+  function sendError (res, next) {
+    res.statusCode = 503
+    res.setHeader('Retry-After', retryAfter)
+    next(serviceUnavailableError)
+  }
+
+  function memoryUsage () {
+    return {
+      eventLoopDelay,
+      rssBytes,
+      heapUsed
+    }
+  }
+
   function onClose (fastify, done) {
-    loopbench.stop()
+    clearInterval(timer)
     done()
   }
 
   next()
+}
+
+function now () {
+  var ts = process.hrtime()
+  return (ts[0] * 1e3) + (ts[1] / 1e6)
 }
 
 module.exports = fp(underPressure, '>=0.27.0')
--- test.js
+++ test.js
@@ -6,11 +6,13 @@ const sget = require('simple-get').concat
 const Fastify = require('fastify')
 const underPressure = require('./index')
 
-test('Should return 503', t => {
+test('Should return 503 on maxEventLoopDelay', t => {
   t.plan(5)
 
   const fastify = Fastify()
-  fastify.register(underPressure)
+  fastify.register(underPressure, {
+    maxEventLoopDelay: 50
+  })
 
   fastify.get('/', (req, reply) => {
     reply.send({ hello: 'world' })
@@ -37,16 +39,80 @@ test('Should return 503', t => {
   })
 })
 
-test('Custom options', t => {
+test('Should return 503 on maxHeapUsedBytes', t => {
   t.plan(5)
 
   const fastify = Fastify()
   fastify.register(underPressure, {
-    maxEventLoopDelay: {
-      limit: 10,
-      retryAfter: 50,
-      message: 'Under pressure!'
-    }
+    maxHeapUsedBytes: 1
+  })
+
+  fastify.get('/', (req, reply) => {
+    reply.send({ hello: 'world' })
+  })
+
+  fastify.listen(0, err => {
+    t.error(err)
+
+    process.nextTick(() => sleep(500))
+    sget({
+      method: 'GET',
+      url: 'http://localhost:' + fastify.server.address().port
+    }, (err, response, body) => {
+      t.error(err)
+      t.strictEqual(response.statusCode, 503)
+      t.strictEqual(response.headers['retry-after'], '10')
+      t.deepEqual(JSON.parse(body), {
+        error: 'Service Unavailable',
+        message: 'Service Unavailable',
+        statusCode: 503
+      })
+      fastify.close()
+    })
+  })
+})
+
+test('Should return 503 on maxRssBytes', t => {
+  t.plan(5)
+
+  const fastify = Fastify()
+  fastify.register(underPressure, {
+    maxRssBytes: 1
+  })
+
+  fastify.get('/', (req, reply) => {
+    reply.send({ hello: 'world' })
+  })
+
+  fastify.listen(0, err => {
+    t.error(err)
+
+    process.nextTick(() => sleep(500))
+    sget({
+      method: 'GET',
+      url: 'http://localhost:' + fastify.server.address().port
+    }, (err, response, body) => {
+      t.error(err)
+      t.strictEqual(response.statusCode, 503)
+      t.strictEqual(response.headers['retry-after'], '10')
+      t.deepEqual(JSON.parse(body), {
+        error: 'Service Unavailable',
+        message: 'Service Unavailable',
+        statusCode: 503
+      })
+      fastify.close()
+    })
+  })
+})
+
+test('Custom message and retry after header', t => {
+  t.plan(5)
+
+  const fastify = Fastify()
+  fastify.register(underPressure, {
+    maxEventLoopDelay: 50,
+    message: 'Under pressure!',
+    retryAfter: 50
   })
 
   fastify.get('/', (req, reply) => {
@@ -69,6 +135,40 @@ test('Custom options', t => {
         message: 'Under pressure!',
         statusCode: 503
       })
+      fastify.close()
+    })
+  })
+})
+
+test('memoryUsage name space', t => {
+  t.plan(8)
+
+  const fastify = Fastify()
+  fastify.register(underPressure, {
+    maxEventLoopDelay: 1000,
+    maxHeapUsedBytes: 100000000,
+    maxRssBytes: 100000000
+  })
+
+  fastify.get('/', (req, reply) => {
+    t.true(fastify.memoryUsage().eventLoopDelay > 0)
+    t.true(fastify.memoryUsage().heapUsed > 0)
+    t.true(fastify.memoryUsage().rssBytes > 0)
+    reply.send({ hello: 'world' })
+  })
+
+  fastify.listen(0, err => {
+    t.error(err)
+    t.is(typeof fastify.memoryUsage, 'function')
+
+    process.nextTick(() => sleep(500))
+    sget({
+      method: 'GET',
+      url: 'http://localhost:' + fastify.server.address().port
+    }, (err, response, body) => {
+      t.error(err)
+      t.strictEqual(response.statusCode, 200)
+      t.deepEqual(JSON.parse(body), { hello: 'world' })
       fastify.close()
     })
   })
