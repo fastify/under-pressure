c184fae207aa2da69640e931de7c0b4764b77e6c
Carlos <karlos1337@gmail.com>
2019-09-24 12:51:24 +0200

    feat(typescript): Add typescript support (#40)

---
+++ index.d.ts
@@ -0,0 +1,29 @@
+import * as http from "http";
+import * as fastify from "fastify";
+
+declare namespace underPressure {
+  interface UnderPressureOptions {
+    maxEventLoopDelay?: number;
+    maxHeapUsedBytes?: number;
+    maxRssBytes?: number;
+    message?: string;
+    retryAfter?: number;
+    healthCheck?: () => Promise<boolean>;
+    healthCheckInterval?: number;
+  }
+}
+
+declare module "fastify" {
+  interface FastifyInstance<HttpServer, HttpRequest, HttpResponse> {
+    memoryUsage: () => string;
+  }
+}
+
+declare let underPressure: fastify.Plugin<
+  http.Server,
+  http.IncomingMessage,
+  http.ServerResponse,
+  underPressure.UnderPressureOptions
+>;
+
+export = underPressure;
