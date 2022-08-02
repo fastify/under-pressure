e24624d666c7a1bff32e086d4ce7c4e5aae27d36
uzlopak <aras.abbasi@googlemail.com>
2022-08-02 11:53:37 +0200

    fix flakyness of tests

---
+++ test/request.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const sget = require('simple-get').concat
+const promisify = require('util').promisify
+const wait = promisify(setTimeout)
+
+const address = process.argv[2]
+const delay = parseInt(process.argv[3])
+
+// custom stringification to avoid circular reference breaking
+function stringifyResponse (response) {
+  return JSON.stringify({
+    statusCode: response.statusCode,
+    headers: response.headers
+  })
+}
+
+async function run () {
+  await wait(delay)
+  sget({
+    method: 'GET',
+    url: address
+  }, function (error, response, body) {
+    if (error instanceof Error) {
+      process.send({
+        error: error.message,
+        response: stringifyResponse(response),
+        body: body.toString()
+      })
+      process.exit(1)
+    }
+
+    process.send({
+      error: null,
+      response: stringifyResponse(response),
+      body: body.toString()
+    })
+    process.exit()
+  })
+}
+
+run()
