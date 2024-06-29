594d42f2664c80937d45bef0c739faa4826202ec
jean <jean.antoine.michelet@gmail.com>
2024-06-29 12:50:35 +0200

    docs: fix typo

--- test/issues/216.test.js
+++ test/issues/216.test.js
@@ -2,7 +2,7 @@ const { test } = require('tap')
 const Fastify = require('fastify')
 const underPressure = require('../../index')
 
-test('onRequest call next if no pression to handle', async t => {
+test('onRequest should call next if no pression to handle', async t => {
   t.plan(1)
 
   const app = Fastify()
@@ -28,9 +28,7 @@ test('should be unhealthy if healthCheck throws an error', async t => {
 
   const app = Fastify()
   app.register(underPressure, {
-    healthCheck: async () => {
-      throw new Error('Kaboom!')
-    },
+    healthCheck: async () => { throw new Error('Kaboom!') },
     healthCheckInterval: 1000,
     exposeStatusRoute: true,
     pressureHandler: (req, rep, type) => {
@@ -49,6 +47,31 @@ test('should be unhealthy if healthCheck throws an error', async t => {
 
   t.equal(response.statusCode, 503)
   t.equal(response.body, 'unhealthy')
+
+  await app.close()
+})
+
+test('should work if pressureHandler is not a function', async t => {
+  t.plan(2)
+
+  const app = Fastify()
+  app.register(underPressure, {
+    healthCheck: async () => { throw new Error('Kaboom!') },
+    healthCheckInterval: 1000,
+    exposeStatusRoute: true,
+    pressureHandler: {}
+  })
+
+  await app.ready()
+
+  const response = await app.inject({
+    method: 'GET',
+    url: '/status'
+  })
+
+  console.log(response.headers)
+  t.equal(response.statusCode, 503)
+  t.equal(response.headers['retry-after'], '10')
 
   await app.close()
 })
