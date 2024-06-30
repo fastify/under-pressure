f63e62d7899cb0d6b885685b2b6bb89247f7c777
Jean <110341611+jean-michelet@users.noreply.github.com>
2024-06-30 10:37:56 +0200

    refactor: allow ErrorConstructor type for customError (#225)

--- types/index.test-d.ts
+++ types/index.test-d.ts
@@ -95,6 +95,17 @@ const server = fastify();
   server.register(fastifyUnderPressure, {
     customError: new Error('custom error message')
   });
+
+  class CustomError extends Error {
+    constructor () {
+      super('Custom error message')
+      Error.captureStackTrace(this, CustomError)
+    }
+  }
+
+  server.register(fastifyUnderPressure, {
+    customError: CustomError
+  });
 };
 
 expectType<'eventLoopDelay'>(fastifyUnderPressure.TYPE_EVENT_LOOP_DELAY)
